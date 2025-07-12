c663ab3415ee59f1e858ddcac9733d931c92d60c
Junior <fabriciojuniorsc@gmail.com>
2025-07-12 14:24:14 -0300

    Hook para busca de contas bancárias

---
+++ extrato-mobile/src/hooks/useListagemContaBancaria.ts
@@ -0,0 +1,32 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useAuth } from "../context/AuthContext";
+import { ApiResponse } from "../types/api-response";
+import { ContaBancaria } from "../types/conta-bancaria";
+
+export const useContasBancarias = (tamanho: number = 10) => {
+    const { session } = useAuth();
+
+    return useInfiniteQuery<ApiResponse<ContaBancaria>>({
+        queryKey: ["contas-bancarias"],
+        queryFn: async ({ pageParam = 0 }) => {
+            if (!session?.access_token) throw new Error("Sessão inválida");
+
+            const res = await fetch(`${process.env.EXPO_PUBLIC_API_URL}/v1/contas-bancarias?pagina=${pageParam}&tamanho=${tamanho}`, {
+                headers: {
+                    Authorization: `Bearer ${session.access_token}`,
+                },
+            });
+
+            if (!res.ok) throw new Error("Erro ao buscar contas bancárias: " + await res.text());
+
+            return await res.json();
+        },
+        getNextPageParam: (lastPage, allPages) => {
+            const nextPage = lastPage.paginaAtual + 1;
+            return nextPage < lastPage.totalPaginas ? nextPage : undefined;
+        },
+        initialPageParam: 0,
+        enabled: !!session?.access_token,
+        staleTime: 1000 * 60 * 5,
+    });
+};
