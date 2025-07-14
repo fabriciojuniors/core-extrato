7070a8acf4d493c999e81f0d106288ca40e734f4
Junior <fabriciojuniorsc@gmail.com>
2025-07-14 19:35:03 -0300

    Listagem de instituições

--- extrato-api/src/main/java/cloud/devjunior/service/InstituicaoFinanceiraService.java
+++ extrato-api/src/main/java/cloud/devjunior/service/InstituicaoFinanceiraService.java
@@ -1,5 +1,7 @@
 package cloud.devjunior.service;
 
+import cloud.devjunior.dto.response.ConsultaPaginadaResponse;
+import cloud.devjunior.dto.response.InstituicaoFinanceiraResponse;
 import cloud.devjunior.entity.InstituicaoFinanceira;
 import cloud.devjunior.repository.InstituicaoFinanceiraRepository;
 import jakarta.enterprise.context.RequestScoped;
@@ -15,5 +17,17 @@ public class InstituicaoFinanceiraService {
     public InstituicaoFinanceira findById(long id) {
         return instituicaoFinanceiraRepository.findByIdOptional(id)
                 .orElseThrow(() -> new NotFoundException("Instituição financeira não encontrada"));
+    }
+
+    public ConsultaPaginadaResponse<InstituicaoFinanceiraResponse> findAll(int pagina, int tamanho) {
+        var consultaPaginada = instituicaoFinanceiraRepository.findAll(pagina, tamanho, null);
+        return new ConsultaPaginadaResponse<>(
+                consultaPaginada.paginaAtual(),
+                consultaPaginada.totalPaginas(),
+                consultaPaginada.totalRegistros(),
+                consultaPaginada.dados().stream()
+                        .map(institucaoFinanceira -> new InstituicaoFinanceiraResponse(institucaoFinanceira.getId(), institucaoFinanceira.getNome()))
+                        .toList()
+        );
     }
 }
